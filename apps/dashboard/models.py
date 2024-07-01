ccb1d4e96c11482a95081bf81acd182fffb51d19
EslamEs1 <eslamdeveloper1@gmail.com>
2024-07-01 03:21:10 +0300

    f

---
+++ apps/dashboard/models.py
@@ -0,0 +1,159 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+# ------------------------Settings
+class Tax(models.Model):
+    number = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.number
+
+
+class BillingMonths(models.Model):
+    date = models.DateField()
+
+    def __str__(self):
+        return self.date
+
+
+# ------------------------People
+class UserType(models.TextChoices):
+    STUDENT = "Student", "الطالب"
+    INSTRUCTOR = "Instructor", "المعلم"
+    MANAGER = "Manager", "المشرف"
+    FAMILIES = "Families", "العائلة"
+    ADMIN = "Admin", "ادمن"
+
+
+class Gender(models.TextChoices):
+    MALE = "Male", "ذكر"
+    FEMALE = "Female", "انثى"
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError("The Phone field must be set")
+        phone = self.normalize_email(phone)
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(phone, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    phone = models.CharField(max_length=15, unique=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    type = models.CharField(
+        choices=UserType.choices, default=UserType.STUDENT, max_length=50
+    )
+    gender = models.CharField(
+        choices=Gender.choices, max_length=10, null=True, blank=True
+    )
+    age = models.IntegerField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    objects = CustomUserManager()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "phone"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone
+
+
+class Manager(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return f"{self.user.name} - Manager"
+
+
+class Instructor(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    qualification = models.CharField(max_length=250)
+    hourly_salary = models.IntegerField(default=0)
+    class_link = models.CharField(max_length=1000)
+    manager = models.ForeignKey(Manager, on_delete=models.SET_NULL)
+    id_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.name} - Instructor"
+
+
+class Families(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    the_state = models.CharField(max_length=250, null=True, blank=True)
+    manager = models.ForeignKey(Manager, on_delete=models.SET_NULL)
+    id_number = models.IntegerField(default="لا يوجد", null=True, blank=True)
+    payment_link = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.user.name} - Instructor"
+
+
+class Student(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    family = models.ForeignKey(Families, on_delete=models.SET_NULL)
+    hourly_salary = models.IntegerField(default=0)
+    payment_link = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.user.name} - Instructor"
+
+
+class Instructor_Student(models.Model):
+    family = models.ForeignKey(Families, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# ------------------------Registration of classes
+
+
+class Evaluation(models.TextChoices):
+    LOW = "Low", "ضعيف"
+    GOOD = "Good", "مقبول"
+    VGOOD = "Vgood", "جيد"
+    EXCELLENCE = "Excellence", "امتياز"
+
+
+class Class(models.Model):
+    family = models.ForeignKey("Families", on_delete=models.CASCADE)
+    student = models.ForeignKey("Student", on_delete=models.CASCADE)
+    instructor = models.ForeignKey("Instructor", on_delete=models.CASCADE)
+    date = models.DateField()
+    number_class_hours = models.IntegerField(default=0)
+    evaluation = models.CharField(choices=Evaluation.choices, default=Evaluation.LOW, max_length=50)
+    subject_name = models.CharField(max_length=300)
+    nb = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Class on {self.date} by {self.instructor.user.name} for {self.student.user.name}"
+
+
+# ------------------------Invoices
+
+
+# ------------------------Advances and discounts
+
+class Advances_Discounts(models.Model):
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=0)
+    comment = models.CharField(max_length=250)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ config/settings.py
@@ -0,0 +1,176 @@
+from pathlib import Path
+import environ
+from django.contrib.messages import constants as messages
+
+env = environ.Env()
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(str(BASE_DIR / ".env"))
+
+
+APPS_DIR = BASE_DIR / "apps"
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.bool("DJANGO_DEBUG", False)
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["127.0.0.1"])
+
+
+# Application definition
+
+DJANGO_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "jazzmin",
+    "django.contrib.admin",
+]
+
+THIRD_PARTY_APPS = [
+    "django_cleanup.apps.CleanupConfig",
+    # "django_summernote",
+    # "crispy_forms",
+]
+
+LOCAL_APPS = [
+    "apps.dashboard",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [str(APPS_DIR / "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_ROOT = str(BASE_DIR / "static")
+STATIC_URL = "/staticfiles/"
+STATICFILES_DIRS = [str(APPS_DIR / "staticfiles")]
+
+
+MEDIA_ROOT = "/home/eslam/media/"
+MEDIA_URL = "/media/"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Quran utl",
+    "site_header": "Quran utl",
+    "site_brand": "Quran utl",
+    "welcome_sign": "Welcome Quran utl",
+    "copyright": "by :Eslam Es",
+    "topmenu_links": [
+        {"name": "Home", "url": "/"},
+    ],
+}
+
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "blue",
+    messages.INFO: "blue",
+    messages.SUCCESS: "green",
+    messages.WARNING: "orange",
+    messages.ERROR: "red",
+}
+
+
+AUTH_USER_MODEL = "apps.dashboard.models.CustomUser"
+
+AUTHENTICATION_BACKENDS = (
+    # "django.contrib.auth.backends.ModelBackend",  # Keep the default backend if needed
+    "apps.dashboard.backends.PhoneBackend",  # Add your custom backend
+)
