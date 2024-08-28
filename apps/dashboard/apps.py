42d46d6aa21ba627f9bc32bcd9d5769ed4392d4c
EslamEs1 <eslamdeveloper1@gmail.com>
2024-08-28 03:31:24 +0300

    changes

--- apps/dashboard/apps.py
+++ apps/dashboard/apps.py
@@ -5,6 +5,5 @@ class DashboardConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.dashboard"
     
-    # def ready(self):
-    #     if not settings.DEBUG:
-    #         import apps.dashboard.signals
+    def ready(self):
+        import apps.dashboard.signals
